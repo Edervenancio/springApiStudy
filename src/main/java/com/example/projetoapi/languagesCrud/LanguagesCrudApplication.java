2cea7ad169363290370d2d23b21adc516168583c
Eder venancio <ederpvenancio@gmail.com>
2023-07-27 19:11:37 -0300

    first commit

---
+++ src/main/java/com/example/projetoapi/languagesCrud/LanguagesCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetoapi.languagesCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguagesCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguagesCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetoapi/languagesCrud/LanguagesCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoapi.languagesCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguagesCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
